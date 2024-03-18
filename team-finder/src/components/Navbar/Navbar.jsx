211fb9d83ef8655fd0536cc11856abc4d0622713
OlivianVarareanu <olivianvarareanu@gmail.com>
2024-03-18 11:02:48 +0200

    package.json proxy added

--- team-finder/src/components/Navbar/Navbar.jsx
+++ team-finder/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import "./Navbar.css"
 import {Link,useNavigate} from 'react-router-dom'
 import Logout from "./Logout"
+import Logo from '/src/assets/Logo.png';
+import ProjectIcon from '/src/assets/project-icon.png';
+import Organization from '/src/assets/team-icon.png';
+import Departments from '/src/assets/department-icon.png';
+import Notifications from '/src/assets/notification-icon.png';
+import Profile from '/src/assets/profile-icon.png';
+import LogoutIcon from '/src/assets/logout-icon.png';
 
 export default function Navbar ()
 {
@@ -16,19 +23,19 @@ export default function Navbar ()
 
                     <ul className="nav-frame">
                         <li className="logo-wrapper">
-                        <img className="logo-image" src="/src/assets/Logo.png" alt="" />
+                        <img className="logo-image" src={Logo} alt="" />
                         </li>
 
                         <li>
                             <Link to="/projects" className="anchor">
-                            <img src="/src/assets/project-icon.png"></img>
+                            <img src={ProjectIcon}></img>
                             <span className="nav-item">PROJECTS</span>
                             </Link>
                         </li>
 
                         <li>
                             <Link to="/team" className="anchor">
-                            <img src="/src/assets/team-icon.png" alt="" />
+                            <img src={Organization} alt="" />
                             <span className="nav-item">ORGANIZATION</span>
                             </Link>
                             
@@ -36,21 +43,21 @@ export default function Navbar ()
 
                         <li>
                             <Link to="/departments" className="anchor">
-                            <img src="/src/assets/department-icon.png" alt="" />
+                            <img src={Departments} alt="" />
                             <span className="nav-item">DEPARTMENTS</span>
                             </Link>
                         </li>
 
                         <li>
                             <Link to="/notifications" className="anchor">
-                            <img src="/src/assets/notification-icon.png" alt="" />
+                            <img src={Notifications} alt="" />
                             <span className="nav-item">NOTIFICATIONS</span>
                             </Link>
                         </li>
 
                         <li>
                             <Link to="/profile" className="anchor">
-                            <img src="/src/assets/profile-icon.png" alt="" />
+                            <img src={Profile} alt="" />
                             <span className="nav-item">PROFILE</span>
                             </Link>
                         </li>
@@ -59,7 +66,7 @@ export default function Navbar ()
                         <li>
 
                         <button className="anchor" onClick={Logout}>
-                        <img className="logout-icon" src="/src/assets/logout-icon.png" alt="" />
+                        <img className="logout-icon" src={LogoutIcon} alt="" />
                         <span className="nav-item">LOG OUT</span>
                         </button>
 
